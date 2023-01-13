ec2185b5d13639bc6884f944ca5969a697fea4d1
Luis Fernando Orjuela <luis.orjuela@rcacontadores.com.co>
2023-01-13 11:46:10 -0500

    todo ok con pagina puglicada con sh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
-import * as path from "path";
+import * as path from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
-},
+    plugins: [react()],
+    base: '/ecommerce_react-tailwind-vite/',
+    resolve: {
+        alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+    },
 })
