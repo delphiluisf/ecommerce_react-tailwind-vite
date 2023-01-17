9d45f3915d2eaaa7a40be7a42f8d31b1a710d818
delphiluisf <69805371+delphiluisf@users.noreply.github.com>
2023-01-17 17:47:37 -0500

    Todo ok hasta el dia 3

--- src/components/Product/col-images/MainImages.jsx
+++ src/components/Product/col-images/MainImages.jsx
@@ -2,34 +2,33 @@ import SlideProduct from '@/components/Product/col-images/SlideProduct'
 import { useState } from 'react'
 
 const MainImages = ({ ARRAY_IMGS, ARRAY_IMG_SMALL }) => {
-
     const [isOpenModal, setIsOpenModal] = useState(false)
 
     const handledOpenModal = () => {
-        setIsOpenModal(true)
+        window.innerWidth > 767 && setIsOpenModal(true)
     }
 
+    const handledCloseModal = () => setIsOpenModal(false)
 
     return (
         <>
-        <SlideProduct
-            ARRAY_IMGS={ARRAY_IMGS}
-            ARRAY_IMG_SMALL={ARRAY_IMG_SMALL} 
-            className='grid md:grid-cols-4 md:gap-4'
-            onClick={handledOpenModal }
-        />
-
-        {/* Componente Modal */}
-        {isOpenModal && (
             <SlideProduct
                 ARRAY_IMGS={ARRAY_IMGS}
-                ARRAY_IMG_SMALL={ARRAY_IMG_SMALL} 
-                isOpenModal = {isOpenModal}
-                className='grid md:grid-cols-4 md:gap-4 md:absolute md:top-1/2 md:left-1/2 md:max-w-md -translate-x-1/2 -translate-y-1/2'
-
+                ARRAY_IMG_SMALL={ARRAY_IMG_SMALL}
+                className='grid md:grid-cols-4 md:gap-4'
+                handledOpenModal={handledOpenModal}
             />
 
-        )}
+            {/* Componente Modal */}
+            {isOpenModal && (
+                <SlideProduct
+                    ARRAY_IMGS={ARRAY_IMGS}
+                    ARRAY_IMG_SMALL={ARRAY_IMG_SMALL}
+                    isOpenModal={isOpenModal}
+                    className='hidden md:absolute md:top-1/2 md:left-1/2 md:grid md:max-w-md md:-translate-x-1/2 md:-translate-y-1/2 md:grid-cols-4 md:gap-4'
+                    handledCloseModal={handledCloseModal}
+                />
+            )}
         </>
     )
 }
--- src/components/Product/col-images/SlideProduct.jsx
+++ src/components/Product/col-images/SlideProduct.jsx
@@ -2,13 +2,23 @@ import { useState } from 'react'
 
 import PrevIcon from '@/components/Icons/PrevIcon'
 import NextIcon from '@/components/Icons/NextIcon'
+import { useRef } from 'react'
+import { useEffect } from 'react'
 
-const SlideProduct = ({ 
-    ARRAY_IMGS = [], 
-    ARRAY_IMG_SMALL = [], 
-    isOpenModal = false, 
-    ...props }) => {
+const SlideProduct = ({
+    ARRAY_IMGS = [],
+    ARRAY_IMG_SMALL = [],
+    isOpenModal = false,
+    handledCloseModal = null,
+    handledOpenModal = () => {},
+    ...props
+}) => {
+    const btnSlider = useRef(null)
     const [index, setIndex] = useState(0)
+
+    // useEffect(() => {
+    //     isOpenModal && btnSlider.current.classList.remove('md:hidden')
+    // }, [isOpenModal])
 
     const handleClickNext = () => {
         index === ARRAY_IMGS.length - 1 ? setIndex(0) : setIndex(index + 1)
@@ -18,17 +28,29 @@ const SlideProduct = ({
     }
 
     return (
-        <section  {...props}>
-            {
-                isOpenModal && <button>Cerrar</button>
-            }
+        <section {...props}>
+            {isOpenModal && (
+                <button
+                    onClick={handledCloseModal}
+                    className='mb-0 text-right font-bold md:col-span-4'
+                >
+                    X
+                </button>
+            )}
             <div className='relative col-span-4'>
                 <img
                     src={ARRAY_IMGS[index]}
                     alt=''
-                    className='aspect-[16/13] w-full md:aspect-[16/18] md:rounded-md'
+                    className='aspect-[16/13] w-full md:aspect-[16/18] md:cursor-pointer md:rounded-md'
+                    onClick={handledOpenModal}
                 />
-                <div className='absolute top-1/2 left-0 flex w-full -translate-y-1/2 justify-between px-4'>
+                <div
+                    // ojo con la desestructuracion no se sabe si funciona
+                    ref={btnSlider}
+                    className={`absolute top-1/2 left-0 flex w-full -translate-y-1/2 justify-between px-4 ${
+                        !isOpenModal && 'md:hidden'
+                    }`}
+                >
                     <button
                         className='grid h-10 w-10 place-items-center rounded-full bg-white'
                         onClick={handleClickPrev}
@@ -43,15 +65,27 @@ const SlideProduct = ({
                     </button>
                 </div>
             </div>
-            {ARRAY_IMG_SMALL.map((smallImg) => (
-                <img
-                    key={smallImg}
-                    src={smallImg}
-                    alt=''
-                    className='hidden md:block md:rounded-md'
-                />
+            {ARRAY_IMG_SMALL.map((smallImg, i) => (
+                <div
+                    key={i}
+                    onClick={() => {
+                        setIndex(i)
+                    }}
+                    className='relative cursor-pointer overflow-hidden rounded-md '
+                >
+                    <img
+                        src={smallImg}
+                        alt=''
+                        className='hidden md:block md:rounded-md'
+                    />
+                    <span
+                        className={`absolute top-0 h-full w-full hover:bg-[rgba(255,255,255,0.7)] ${
+                            i === index &&
+                            'border-4 border-orange-500 bg-[rgba(255,255,255,0.7)]'
+                        }`}
+                    ></span>
+                </div>
             ))}
-           
         </section>
     )
 }
